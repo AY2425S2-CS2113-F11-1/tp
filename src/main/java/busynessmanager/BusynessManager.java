e115619de030502f00398e258e5f8e04ae718632
Rozalie Smit <rozaliesmit@gmail.com>
2025-03-18 14:50:02 +0800

    SalesManager update

--- src/main/java/busynessmanager/BusynessManager.java
+++ src/main/java/busynessmanager/BusynessManager.java
@@ -1,7 +1,5 @@
 package busynessmanager;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class BusynessManager {
--- src/main/java/busynessmanager/SalesManager.java
+++ src/main/java/busynessmanager/SalesManager.java
@@ -1,15 +1,21 @@
 package busynessmanager;
 
+/**
+ * Updates sales transactions, decrements inventory and increases quantity sold
+ */
 public class SalesManager {
     private InventoryManager inventory;
 
     public SalesManager(InventoryManager inventory) {
-            this.inventory = inventory;
-        }
+        this.inventory = inventory;
+    }
 
     /**
-     *  Record a sale of a quantity of a product by its ID
-      */
+     * Record a sale of a quantity of a product by its ID
+     *
+     * @param id Product ID
+     * @param qtySold Quantity of product sold
+     */
     public void recordSale(String id, int qtySold) {
         if (qtySold <= 0) {
             System.out.println("Quantity sold must be greater than 0.");
@@ -21,10 +27,11 @@ public class SalesManager {
 
     /**
      * Clear sales for a product by its ID
+     *
      * @param id product ID
      */
     public void clearSales(String id) {
         inventory.resetProductSales(id);
         System.out.println("Sales cleared: Product ID " + id);
-        }
     }
+}
