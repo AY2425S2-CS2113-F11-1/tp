e8b22e94111affd8dd0b2a351b657ef2ba89655a
b1inmeister <jlmk2102@gmail.com>
2025-03-18 12:01:39 +0800

    Rename custom exceptions and modify InvalidStringException
    The custom exceptions are not named as exceptions. Moreover, the
    message printed in InvalidString is repeated in all scenarios.
    
    Adding the word "Exception" to the custom exceptions will
    explicitly inform others that they are exceptions. Removing the
    message parameter will help to simplify the code.
    
    Let's,
    * Rename all custom exceptions and their class files to include
    the word "Exception"
    * Modify InvalidString such that there is no more message
    parameter in the constructor.

---
+++ src/main/java/busynessmanager/exceptions/InvalidStringException.java
@@ -0,0 +1,15 @@
+package busynessmanager.exceptions;
+
+/**
+ * Exception for an invalid String.
+ */
+public class InvalidStringException extends Exception {
+
+
+    /**
+     * Constructs the InvalidString exception.
+     */
+    public InvalidStringException() {
+        super("Input String cannot be split.");
+    }
+}
