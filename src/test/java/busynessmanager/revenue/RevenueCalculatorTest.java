06dd90546e5ca6c523a1302c3e3904780f2bfa8c
b1inmeister <jlmk2102@gmail.com>
2025-04-06 19:50:39 +0800

    Troubleshoot Gradle check
    The Gradle check is failing, as the JUnit tests are failing.
    
    Checking the JUnit tests will ensure that the methods in our code
    work as expected.
    
    Let's check the JUnit tests and modify them if necessary.

--- src/test/java/busynessmanager/revenue/RevenueCalculatorTest.java
+++ src/test/java/busynessmanager/revenue/RevenueCalculatorTest.java
@@ -16,9 +16,9 @@ public class RevenueCalculatorTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        im = new InventoryManager();
-        sm = new SalesManager(im);
-        rc = new RevenueCalculator(sm);
+        this.im = new InventoryManager();
+        this.sm = new SalesManager(im);
+        this.rc = new RevenueCalculator(sm);
     }
 
     @Test
@@ -26,25 +26,25 @@ public class RevenueCalculatorTest {
         im.addProduct("bean", 100, 0.6);
         im.addProduct("donkey", 6000, 900);
         im.addProduct("bingbangbong", 1, 10000);
-        sm.recordSale("ID_0001", 50);
-        sm.recordSale("ID_0002", 10);
-        sm.recordSale("ID_0003", 1);
+        sm.recordSale("ID_0010", 50);
+        sm.recordSale("ID_0011", 10);
+        sm.recordSale("ID_0012", 1);
 
         im.printProducts();
 
         assertEquals(
             30,
-            rc.computeProductRevenue("ID_0001")
+            rc.computeProductRevenue("ID_0010")
         );
 
         assertEquals(
             9000,
-            rc.computeProductRevenue("ID_0002")
+            rc.computeProductRevenue("ID_0011")
         );
 
         assertEquals(
             10000,
-            rc.computeProductRevenue("ID_0003")
+            rc.computeProductRevenue("ID_0012")
         );
     }
 
@@ -53,9 +53,9 @@ public class RevenueCalculatorTest {
         im.addProduct("bean", 100, 0.6); // ID_0010
         im.addProduct("donkey", 6000, 900);
         im.addProduct("bingbangbong", 1, 10000);
-        sm.recordSale("ID_0004", 50);
-        sm.recordSale("ID_0005", 10);
-        sm.recordSale("ID_0006", 1);
+        sm.recordSale("ID_0013", 50);
+        sm.recordSale("ID_0014", 10);
+        sm.recordSale("ID_0015", 1);
 
         assertEquals(
             19030,
